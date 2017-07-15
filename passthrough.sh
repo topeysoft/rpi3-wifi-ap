89c3db48dcb14830c5f374a3c42c42884264c200
Temitope Adeyeri <temi.adeyeri@gmail.com>
2017-07-15 13:18:47 -0500

    .

---
+++ passthrough.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root"
+	exit
+fi
+
+ADAPTER="wlan0"
+
+# Allow overriding from eth0 by passing in a single argument
+if [ $# -eq 1 ]; then
+    ADAPTER="$1"
+fi
+
+#Uncomment net.ipv4.ip_forward
+sed -i -- 's/#net.ipv4.ip_forward/net.ipv4.ip_forward/g' /etc/sysctl.conf
+#Change value of net.ipv4.ip_forward if not already 1
+sed -i -- 's/net.ipv4.ip_forward=0/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+#Activate on current system
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -t nat -A POSTROUTING -o $ADAPTER -j MASQUERADE  
+iptables -A FORWARD -i $ADAPTER -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+iptables -A FORWARD -i wlan0 -o $ADAPTER -j ACCEPT
+
+iptables-restore < /etc/iptables.ipv4.nat  
--- setup.sh
+++ setup.sh
@@ -16,68 +16,12 @@ if [[ $# -lt 1 ]];
 fi
 
 APPASS="$1"
-APSSID="rPi3"
+APSSID="ElyirHub.AP"
+ADAPTER="wlan0"
 
 if [[ $# -eq 2 ]]; then
 	APSSID=$2
 fi
 
-apt-get remove --purge hostapd -y
-apt-get install hostapd dnsmasq -y
-
-cat > /etc/systemd/system/hostapd.service <<EOF
-[Unit]
-Description=Hostapd IEEE 802.11 Access Point
-After=sys-subsystem-net-devices-wlan0.device
-BindsTo=sys-subsystem-net-devices-wlan0.device
-
-[Service]
-Type=forking
-PIDFile=/var/run/hostapd.pid
-ExecStart=/usr/sbin/hostapd -B /etc/hostapd/hostapd.conf -P /var/run/hostapd.pid
-
-[Install]
-WantedBy=multi-user.target
-
-EOF
-
-cat > /etc/dnsmasq.conf <<EOF
-interface=wlan0
-dhcp-range=10.0.0.2,10.0.0.5,255.255.255.0,12h
-EOF
-
-cat > /etc/hostapd/hostapd.conf <<EOF
-interface=wlan0
-hw_mode=g
-channel=10
-auth_algs=1
-wpa=2
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=CCMP
-rsn_pairwise=CCMP
-wpa_passphrase=$APPASS
-ssid=$APSSID
-EOF
-
-sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
-sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
-sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
-
-cat >> /etc/network/interfaces <<EOF
-	wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
-
-# Added by rPi Access Point Setup
-allow-hotplug wlan0
-iface wlan0 inet static
-	address 10.0.0.1
-	netmask 255.255.255.0
-	network 10.0.0.0
-	broadcast 10.0.0.255
-
-EOF
-
-echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
-
-systemctl enable hostapd
-
-echo "All done! Please reboot"
+sudo bash create-ap.sh $APPASS $APSSID
+sudo bash passthrough.sh $ADAPTER
